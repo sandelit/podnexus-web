0436da5bff051cb3a6b905eb3f253d75ef8b3853
sandelit <thomas.sandelin@gmail.com>
2024-07-18 16:22:22 +0300

    add login page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,30 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
   return (
-
-  <header className="flex w-screen items-center justify-between h-24 text-white bg-gray-700">
-    <div className="flex gap-4 ml-12">
-      <h2>
-        <a href="/">Logo</a>
-      </h2>
-      <h2>
-        <a href="/dashboard">Dashboard</a>
-      </h2>
-      <h2>
-        <a href="/pricing">Pricing</a>
-      </h2>
-    </div>
-    <div className="flex items-center gap-4 mr-12">
-      <h2>
-        Schedule a Demo
-      </h2>
-      <h2>
-        Log In
-      </h2>
-      <Button>
-        Get Started for Free
-      </Button>
-    </div>
-  </header>
-  )
-  
+    <header className="flex w-screen items-center justify-between h-24 text-white bg-gray-700">
+      <div className="flex gap-4 ml-12">
+        <h2>
+          <a href="/">Logo</a>
+        </h2>
+        <h2>
+          <a href="/dashboard">Dashboard</a>
+        </h2>
+        <h2>
+          <a href="/pricing">Pricing</a>
+        </h2>
+      </div>
+      <div className="flex items-center gap-4 mr-12">
+        <h2>
+          Schedule a Demo
+        </h2>
+        <h2>
+          <a href="/login">Log in</a>
+        </h2>
+        <Button>
+          <a href="/register">Get started for free</a>
+        </Button>
+      </div>
+    </header>
+  );
 }
